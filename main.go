e7ff450f5b6b793be240750c558cac5de5470b27
Ajay Singh <ajay@nrb.org.np>
2025-06-25 15:01:02 +0545

    setup for the server and config in progress

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/ajaysinghnp/dcabinet/lib/config"
+)
 
 func main() {
-	fmt.Println("Digital Cabinet API is running successfully!")
+	// load config
+	config := config.MustLoad()
+
+	// database setup
+
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome to DCabinet!"))
+	})
+
+	// setup server
+	fmt.Println("Starting server on", config.HTTPServer.Host+":"+string(config.HTTPServer.Port))
+	server := &http.Server{
+		Addr:    config.HTTPServer.Host + ":" + string(config.HTTPServer.Port),
+		Handler: router,
+	}
+	// start server
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal("Failed to start server: ", err)
+	}
+
 }
